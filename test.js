76d2fccd855237e73243fbb87d174a7ae60ff61c
Mark Greenwald <Mark@Greenwald>
2025-08-11 17:33:53 -0700

    test: improved warning visibility

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ process.on('unhandledRejection', (reason, promise) => { unhandledRejection(reaso
 process.on('uncaughtException', (error) => { unhandledRejection(error); });
 
 console.info(`\x1b[47m
-  Running Test(s)\r\x1b[0m
+  Running Test(s)\x1b[0m
 `);
 
 const search = [root];
@@ -67,16 +67,14 @@ while (search.length) {
               resolve();
             });
           } catch (error) { test.errors.push(error?.message ?? `${error ?? 'Unknown Error'}`); }
-          if (!test.asserts.length) { 
-            suite.failed = true;
-            test.warns.push(`${warn} No assertions made.`); 
-          }
+          if (!test.asserts.length) { test.warns.push(`${warn} No assertions made.`); }
           const failures = test.asserts.filter(a => !a.assertion).map(a => a.message).concat(test.errors);
-          if (failures.length) {
-            suite.failed = true;
-            console.log(`  ${fail} ${test.name}`);
-            for (const failure of failures) { console.error(`    \x1b[31m${failure}\x1b[0m`); }
-          } else { console.log(`  ${pass} ${test.name}`); }
+          if (failures.length) { console.log(`  ${fail} ${test.name}`); } 
+          else if (test.warns.length) { console.log(`  ${warn} ${test.name}`); } 
+          else { console.log(`  ${pass} ${test.name}`); }
+          if (failures.length || test.errors.length || test.warns.length) { suite.failed = true; }
+          for (const failure of failures) { console.error(`    \x1b[31m${failure}\x1b[0m`); }
+          for (const warn of test.warns) { console.warn(`    \x1b[33m${warn}\x1b[0m`); }
         }
       } catch (error) { 
         suite.failed = true;
